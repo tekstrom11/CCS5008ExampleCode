445d6153a855b53d2178fb8397dc5bdd8b4f6a0f
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-02 17:02:06 -0600

    update from slide deck

---
+++ HelloC/sampleCode/helloc_slides.c
@@ -0,0 +1,87 @@
+/* 
+ 
+ Printing Aloha, World in C.
+ This is also a multi-line comment.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void inputExample() {
+    int x = 0;
+    printf("Enter a number: ");
+    scanf("%d", &x); // &x is the address of x
+    printf("Aloha, World! %d is the answer...\n", x);
+}
+
+void forExample() {
+    for (int i = 0; i < 5; i++) {
+        printf("Aloha, World!\n");
+    }
+
+    for (int i = 5; i > 0; i--) {
+        printf("Aloha, World!\n");
+        if (i == 2) {
+            break; // exit the loop
+        }
+    }
+
+    for (int i = 0; i < 5; i++) {
+        if (i == 2) {
+            continue; // skip the rest of the loop
+        }
+        printf("Aloha, World!\n");
+    }
+
+
+}
+
+void whileExample() {
+    int i = 0; // declare and initialize i
+    while (i < 5) {
+        printf("Aloha, World!\n");
+        i++; // similar as i = i + 1
+        // i += 1; is also good here
+    }
+
+    while(i) { // this works since 0 is false
+        printf("Aloha, World!\n");
+        i--; // similar as i = i - 1
+        // i -= 1; is also good here
+    } 
+}
+
+void conditionExample() {
+    int x = 0;
+    if (x == 0) {
+        printf("Aloha, World!\n");
+    } else {
+        printf("Ciao, World!\n");
+    }
+
+    x = 2;
+    int y = 0;
+    if (x > 1 && y < 2) {
+        printf("Aloha, World!\n");
+    } else if (x > 1 || y < 2) {
+        printf("Ciao, World!\n");
+    } else {
+        printf("Hola, World!\n");
+    }
+}
+
+int myAddition(int a, int b) {
+    return a + b; 
+}
+
+void printHelloWorld() {
+    printf("Aloha, World!\n");
+}
+
+int main() {
+    int x = 40 + 2;     // This is a single comment
+    printf("Aloha, World! %d is the answer...\n", x);
+    sizeExample();
+    return 0;
+}
---
+++ HelloC/sampleCode/helloc_slides.py
@@ -0,0 +1,9 @@
+
+
+# Printing Aloha, World!  in python
+
+def main():
+    x = 40 + 2 # this is a comment
+    print("Aloha, World! %d is the answer..." % x)
+
+main() 
