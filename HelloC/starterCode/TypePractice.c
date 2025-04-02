a053480f16fa274857cf651b9ec612c130a50f86
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-02 17:14:13 -0600

    initial starter code

---
+++ HelloC/starterCode/TypePractice.c
@@ -0,0 +1,19 @@
+/** 
+ * Starter code for TypePractice.c
+ * 
+ * This program demonstrates the use of various data types in C.
+ * 
+ * 
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h> // for bool type
+#include <stdint.h> // for uint64_t type
+
+
+
+int main() {
+
+    return 0;
+}
