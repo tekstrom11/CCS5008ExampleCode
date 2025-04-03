c644e396325e1322577564bf31cf706173cfd467
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-02 19:22:14 -0600

    removed long long

--- HelloC/solutionCode/TypePractice.c
+++ HelloC/solutionCode/TypePractice.c
@@ -18,7 +18,6 @@ void sizeExample() {
     printf("Size of int: %zu bytes\n", sizeof(int));
     printf("Size of short: %zu bytes\n", sizeof(short));
     printf("Size of long: %zu bytes\n", sizeof(long));
-    printf("Size of long long: %zu bytes\n", sizeof(long long));
     printf("Size of float: %zu bytes\n", sizeof(float));
     printf("Size of double: %zu bytes\n", sizeof(double));
     printf("Size of long double: %zu bytes\n", sizeof(long double));
