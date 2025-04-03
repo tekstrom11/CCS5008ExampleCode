2ab19564e15798d3b11b47523dfc8b843a92bad1
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-02 19:39:36 -0600

    updated from code along

--- HelloC/solutionCode/TypePractice.c
+++ HelloC/solutionCode/TypePractice.c
@@ -11,25 +11,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h> // for bool type
-#include <stdint.h>
+#include <stdint.h> // for uint64_t type
 
-void sizeExample() { 
-    printf("Size of char: %zu bytes\n", sizeof(char));
-    printf("Size of int: %zu bytes\n", sizeof(int));
-    printf("Size of short: %zu bytes\n", sizeof(short));
-    printf("Size of long: %zu bytes\n", sizeof(long));
-    printf("Size of float: %zu bytes\n", sizeof(float));
-    printf("Size of double: %zu bytes\n", sizeof(double));
-    printf("Size of long double: %zu bytes\n", sizeof(long double));
+void sizeExample() {
+    // sizeof(type or variable)
+    //char myLetter = 'H';
+    printf("Size of char:  %zu bytes\n", sizeof(char));
+    printf("Size of int:  %zu bytes\n", sizeof(int));
+    printf("Size of short:  %zu bytes\n", sizeof(short));
+    printf("Size of long:  %zu bytes\n", sizeof(long));
+    // floating point numbers also called decimals
+    printf("Size of float:  %zu bytes\n", sizeof(float));
+    printf("Size of double:  %zu bytes\n", sizeof(double));
+    printf("Size of long double:  %zu bytes\n", sizeof(long double));
 
     // now some special types
-    printf("Size of bool: %zu bytes\n", sizeof(bool)); 
-    printf("Size of uint64_t: %zu bytes\n", sizeof(uint64_t));
+    //bool x = true;
+    printf("Size of bool: %zu bytes\n", sizeof(bool));
+    printf("Size of uint64_t:  %zu bytes\n", sizeof(uint64_t));
 
 }
 
-
 int main() {
     sizeExample();
-    return 0; 
+    return 0;
 }
