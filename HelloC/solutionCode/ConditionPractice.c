a01f5e19059cd828db946960e4277009cf571a44
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-03 16:24:47 -0600

    updated with code from code along

--- HelloC/solutionCode/ConditionPractice.c
+++ HelloC/solutionCode/ConditionPractice.c
@@ -9,13 +9,12 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h> 
 
 void simpleConditionPractice(int lower, int upper) {
     // if statement
-    if (lower < upper) {
+    if(lower < upper) {
         printf("Lower is less than upper\n");
-    } else if (lower > upper) {
+    } else if(lower > upper) {
         printf("Lower is greater than upper\n");
     } else {
         printf("Lower is equal to upper\n");
@@ -27,21 +26,22 @@ void simpleConditionPractice(int lower, int upper) {
 }
 
 void complexConditionPractice(int lower, int upper) {
-    // if statement with logical operators
-    if (lower < upper && lower > 0) {
+    // if with logical operators
+    if(lower < upper && lower > 0) { // 0 < lower < upper <- not allowed!
         printf("Lower is less than upper and greater than 0\n");
-    } else if (lower > upper || lower < 0) {
+    } else if(lower > upper || lower < 0) {
         printf("Lower is either greater than upper or less than 0\n");
-    } else {
-        printf("Lower is equal to upper and greater than or equal to 0\n");
+    }else {
+        printf("Lower if equal to upper and greater than or equal to 0\n");
     }
 
-    // just using int values
+    // just using in values
     if(lower - upper) {
         printf("Lower is not equal to upper\n");
-    } else {
+    }else {
         printf("Lower is equal to upper\n");
     }
+
 }
 
 
@@ -49,20 +49,18 @@ int main() {
     int lower = 5;
     int upper = 10;
 
-    printf("Simple Condition Practice with (%d, %d):\n", lower, upper);
+    printf("Simple Condition Practice with (%d, %d)\n", lower, upper);
     simpleConditionPractice(lower, upper);
-
-    printf("Complex Condition Practice with (%d, %d): \n", lower, upper);
     complexConditionPractice(lower, upper);
-
-    printf("\n");
 
     lower = 10;
     upper = 10;
 
-    printf("Simple Condition Practice (%d, %d):\n", lower, upper);
+    printf("Simple Condition Practice with (%d, %d)\n", lower, upper);
     simpleConditionPractice(lower, upper);
-    printf("Complex Condition Practice (%d, %d):\n", lower, upper);
     complexConditionPractice(lower, upper);
+
+
+    printf("\n");
     return 0;
 }
