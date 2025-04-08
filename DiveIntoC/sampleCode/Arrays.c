e49b3435f08021ef614b321e0e7a91c6ba08f88a
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-07 20:06:11 -0600

    updated matrix math

---
+++ DiveIntoC/sampleCode/Arrays.c
@@ -0,0 +1,72 @@
+/**
+ * Example using simple array declaration. 
+ * 
+ * @author Albert Lionelle
+ */
+#include <stdio.h>
+
+
+void printMatrix(int* matrix, int width, int height) {
+    for(int i = 0; i < height; i++) {
+        for(int j = 0; j < width; j++){
+            printf("%3d ", matrix[i * width + j]);
+        }
+        printf("\n");
+    }
+}
+
+void matrixExample() {
+    int matrix[3][3];
+    int counter = 0;
+    for(int i = 0; i < 3; i++) {
+        for(int j = 0; j < 3; j++){
+            matrix[i][j] = counter++;
+        }
+    }
+    printMatrix((int*)matrix, 3, 3);
+
+    int matrix2[3][4] = {
+        {1, 2, 3, 4},
+        {5, 6, 7, 8},
+        {9, 10, 11, 12}
+    };
+
+    printMatrix((int*)matrix2, 4, 3);
+}
+
+
+
+void forLoopExample() {
+    int evens[100];
+
+    for(int i = 0; i < 100; i++) {
+        evens[i] = i * 2;
+    }
+    for(int i = 0; i < 100; i++) {
+        printf("%d ", evens[i]);
+    }
+    printf("\n");
+}
+
+
+int main() {
+    int points[2] = {10, 20};
+    printf("Point 0: %d\n", points[0]);
+    printf("Point 1: %d\n", points[1]);
+
+    for(int i = 0; i < 2; i++) {
+        printf("Points value %d, points pointer %p\n", points[i], (void*)&points[i]);
+    }
+
+    int* pointers = points;
+    for(int i =0; i < 2; i++) {
+        printf("Points value %d, points pointer %p\n", *(pointers+i), (void*)(pointers+i));
+    }
+
+    // int points2[2];
+    // points2[0] = 10;
+    // points2[1] = 20;
+
+    forLoopExample();
+    matrixExample();
+}
