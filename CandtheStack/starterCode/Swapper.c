228346c5cb94343aa50b123b91f148f96efe5476
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-08 15:13:47 -0600

    set up starter code

---
+++ CandtheStack/starterCode/Swapper.c
@@ -0,0 +1,19 @@
+/**
+ * Initial Starter code for Code Along: Swapper. 
+ * This code will demonstrate how to swap two numbers using a pointers.
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main() {
+    int num1 = 10;
+    int num2 = 20;
+    printf("Values: num1 = %d, num2 = %d\n", num1, num2);
+    
+    printf("Swapped values: num1 = %d, num2 = %d\n", num1, num2);
+    return 0;
+}
