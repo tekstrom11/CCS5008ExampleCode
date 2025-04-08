e190d8029adfe2135cd1c783c91f3ffe7b1f6bad
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-08 14:17:42 -0600

    solution updated

--- CandtheStack/solutionCode/Swapper.c
+++ CandtheStack/solutionCode/Swapper.c
@@ -4,3 +4,22 @@
  * @author Albert Lionelle
  * @date 2024-04-08
  */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+int main() {
+    int num1 = 10;
+    int num2 = 20;
+    printf("Values: num1 = %d, num2 = %d\n", num1, num2);
+    swap(&num1, &num2);
+    printf("Swapped values: num1 = %d, num2 = %d\n", num1, num2);
+    return 0;
+}
