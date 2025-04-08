ba2b020ca5cbcafdcaa570fc0016035c49a0fc29
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-08 14:20:31 -0600

    added solution

---
+++ CandtheStack/solutionCode/ProgramArguments.c
@@ -0,0 +1,29 @@
+/**
+ * This code along demonstrates the use of program arguments in C programming.
+ *
+ * In this program, we will access command line arguments and display them.
+ *
+ * @author Albert Lionelle
+ * @date 2024-04-08
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char const *argv[]) {
+  printf("Number of arguments: %d\n", argc);
+  printf("Arguments:\n");
+  for (int i = 0; i < argc; i++) {
+    printf("argv[%d]: %s\n", i, argv[i]);
+  }
+  if (argc < 1) {
+    printf("No arguments provided.\n");
+    return 1;
+  }
+  if (argc > 3) {
+    printf("First argument as integer: %d\n", atoi(argv[1]));
+    printf("Second argument as string: %s\n", argv[2]);
+  }
+
+  return 0;
+}
