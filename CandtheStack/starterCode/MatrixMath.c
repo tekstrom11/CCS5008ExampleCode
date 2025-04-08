4d8534b739c0bcaf58799c87689a5dee36c2d4f7
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-08 15:19:09 -0600

    initial starter code

---
+++ CandtheStack/starterCode/MatrixMath.c
@@ -0,0 +1,8 @@
+/**
+ * Initial starter code for Code Along Matrix Math. 
+ * 
+ * In this example, we will use both structs and arrays
+ * to help represent 3D matrices. 
+ */
+#include <stdio.h>
+#include <stdlib.h>
