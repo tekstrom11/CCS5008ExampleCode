a13185f9a8ee00e77f0997915a49a059f7f68988
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-08 15:15:31 -0600

    Added starter code

---
+++ CandtheStack/starterCode/ArrayPractice.c
@@ -0,0 +1,14 @@
+/**
+ * Starter File for code along Array Practice. 
+ * This file will demonstrate basic array operations.
+ * This will include functions for initializing, printing, and manipulating arrays.
+ * 
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main() {
+    
+}
