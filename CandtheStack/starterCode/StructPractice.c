c11b845a3f2b8186a5a07665f551709e8f26c9e2
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-08 15:17:04 -0600

    initial starter code

---
+++ CandtheStack/starterCode/StructPractice.c
@@ -0,0 +1,9 @@
+/**
+ * Starter code for Code Along Struct Practice.
+ * In this code along we will explore structs, initializing
+ * and using pointers to structs for access in functions.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
