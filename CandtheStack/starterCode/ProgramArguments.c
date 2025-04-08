19f238b910a4c746088d58c09ffae4855d0b7bc3
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-08 17:12:47 -0600

    initial code

---
+++ CandtheStack/starterCode/ProgramArguments.c
@@ -0,0 +1,11 @@
+/**
+ * Starter code for Code Along: Command Line Arguments.
+ *
+ * In this code will explore access the the
+ * program command line arguments.
+ */
+
+int main(int argc, char const *argv[]) {
+    
+  return 0;
+}
