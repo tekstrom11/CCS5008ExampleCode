17de3004a8755280be27601c906ab69013972ee0
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-08 18:41:19 -0600

    Updated with code along

--- CandtheStack/solutionCode/StructPractice.c
+++ CandtheStack/solutionCode/StructPractice.c
@@ -28,21 +28,21 @@ void printBook(Book *b) {
   printf("Title: %s, Author: %s, Year: %d\n", b->title, b->author, b->year);
 }
 
-void updatePersonAge(Person *p, int newAge) { p->age = newAge; }
-
-void updateBookTitle(Book *b, const char *newTitle) {
-  strncpy(b->title, newTitle, sizeof(b->title) - 1);
-  b->title[sizeof(b->title) - 1] = '\0'; // Ensure null termination
-}
+void updateAge(Person *p, int newAge) { p->age = newAge; }
 
 int checkDrinkingAge(Person *p) {
   if (p->age >= 21) {
-    printf("%s is of legal drinking age.\n", p->name);
-    return 1; // true
+    printf("%s is of legal drinking (US) age.\n", p->name);
+    return 1;
   } else {
-    printf("%s is not of legal drinking age.\n", p->name);
-    return 0; // false
+    printf("%s is not of legal drinking (US) age.\n", p->name);
+    return 0;
   }
+}
+
+void updateBookTitle(Book *b, const char *newTitle) {
+  strncpy(b->title, newTitle, sizeof(b->title) - 1);
+  b->title[sizeof(b->title) - 1] = '\0';
 }
 
 int main() {
@@ -50,36 +50,29 @@ int main() {
   strcpy(person1.name, "Alice");
   person1.age = 25;
   printPerson(&person1);
-  updatePersonAge(&person1, 30);
+  updateAge(&person1, 30);
   printPerson(&person1);
   checkDrinkingAge(&person1);
 
   Person person2 = {"Bob", 18};
   printPerson(&person2);
-  updatePersonAge(&person2, 20);
+  updateAge(&person2, 20);
   printPerson(&person2);
   checkDrinkingAge(&person2);
 
+  printf("\n");
   Person people[2] = {person1, person2};
   for (int i = 0; i < 2; i++) {
     printPerson(&people[i]);
     checkDrinkingAge(&people[i]);
   }
 
-  Person people2[2] = {{"Alice", 25}, {"Bob", 18}};
-  for (int i = 0; i < 2; i++) {
-    printPerson(&people2[i]);
-    checkDrinkingAge(&people2[i]);
-  }
-
+  printf("\n");
   Book book1 = {"The Great Gatsby", "F. Scott Fitzgerald", 1925};
   printBook(&book1);
-
   updateBookTitle(&book1, "The Great Gatsby (Updated Edition)");
   printBook(&book1);
 
-  Book book2 = {"1984", "George Orwell", 1949};
-  printBook(&book2);
-
+  // Person people2[2] = {{"Alice", 25}, {"Bob", 18}};
   return 0;
 }
