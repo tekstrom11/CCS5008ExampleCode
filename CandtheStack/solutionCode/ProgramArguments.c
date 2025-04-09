3c256019c10cbb2dc9620be207182af6f0263a1e
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-08 18:20:20 -0600

    updated from code along

--- CandtheStack/solutionCode/ProgramArguments.c
+++ CandtheStack/solutionCode/ProgramArguments.c
@@ -14,16 +14,14 @@ int main(int argc, char const *argv[]) {
   printf("Number of arguments: %d\n", argc);
   printf("Arguments:\n");
   for (int i = 0; i < argc; i++) {
-    printf("argv[%d]: %s\n", i, argv[i]);
+      printf("argv[%d]: %s\n", i, argv[i]);
   }
-  if (argc < 1) {
-    printf("No arguments provided.\n");
-    return 1;
-  }
-  if (argc > 3) {
-    printf("First argument as integer: %d\n", atoi(argv[1]));
-    printf("Second argument as string: %s\n", argv[2]);
+  printf("\n");
+  if (argc > 2) {
+      printf("The first arg as int: %d\n", atoi(argv[1]));
+      printf("The second arg as string: %s\n", argv[2]);
   }
 
-  return 0;
+return 0;
 }
+
