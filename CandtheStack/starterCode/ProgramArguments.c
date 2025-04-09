b5d48834af5f17dbddd2df66f3398b667f9fb323
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-08 18:12:17 -0600

    starter code

--- CandtheStack/starterCode/ProgramArguments.c
+++ CandtheStack/starterCode/ProgramArguments.c
@@ -4,8 +4,10 @@
  * In this code will explore access the the
  * program command line arguments.
  */
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char const *argv[]) {
-    
+
   return 0;
 }
