34c22ed6e973deb8b328d533b9161bf143ba6664
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-09 23:23:11 -0600

    updated examples

--- CandtheHeap/sampleCode/Heap.c
+++ CandtheHeap/sampleCode/Heap.c
@@ -6,15 +6,28 @@
  * @date 2025.04.09
  */
 
-
+#include <stdio.h>
+#include <stdlib.h>
 
 int* newArray(int size) {
     //int arr_stack[size]; 
     int *arr_heap = (int *) malloc(sizeof(int) * 5);
+    for(int i = 0; i < size; i++) {
+        arr_heap[i] = (i+1) * 2;
+    }
     return arr_heap;
 }
 
-int main(){
+void buildPrint(int index) {
     int *arr = newArray(5);
+    printf("arr[%d] = %d\n", index, arr[index]);
+    free(arr);
+}
+
+int main(){
+    int index = 0;
+    buildPrint(index);
     return 0;
 }
+
+
