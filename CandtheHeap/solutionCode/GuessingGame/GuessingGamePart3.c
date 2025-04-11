2adab58ef086a3d2f5ed9fcd1c01182247e969eb
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-10 21:40:28 -0600

    updated from code along

--- CandtheHeap/solutionCode/GuessingGame/GuessingGamePart3.c
+++ CandtheHeap/solutionCode/GuessingGame/GuessingGamePart3.c
@@ -1,160 +1,155 @@
 /**
  * Solution for Code Along Guessing Game Part 3
- *  
+ *
  */
 
+#include <stdbool.h> // for bool type
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h> // for bool type
-#include <time.h>   // for random number generation
 #include <string.h> // for string manipulation
+#include <time.h>   // for random number generation
+
 
 #include "GuessingGame.h" // Include the header file for function declarations
 
 int getRandomNumber(int min, int max) {
-    // Generate a random number between min and max
-    return (rand() % (max - min + 1)) + min;
+  // Generate a random number between min and max
+  return (rand() % (max - min + 1)) + min;
 }
 
-
-GameState* initializeGameState(int maxAttempts, int playerType, int min, int max) {
-    GameState *gameState = (GameState *)malloc(sizeof(GameState));
-    gameState->guesses = (int *)malloc(sizeof(int) * maxAttempts);
-    gameState->attempts = 0;
-    gameState->maxAttempts = maxAttempts;
-    gameState->playerType = playerType;
-    gameState->min = min;
-    gameState->max = max;
-    return gameState;
+GameState *initializeGameState(int maxAttempts, int playerType, int min,
+                               int max) {
+  GameState *gameState = (GameState *)malloc(sizeof(GameState));
+  gameState->guesses = (int *)malloc(sizeof(int) * maxAttempts);
+  gameState->attempts = 0;
+  gameState->maxAttempts = maxAttempts;
+  gameState->playerType = playerType;
+  gameState->min = min;
+  gameState->max = max;
+  return gameState;
 }
 
 void freeGameState(GameState *gameState) {
-    free(gameState->guesses);
-    free(gameState);
-}
-
-
-void giveFeedback(int randomNumber, bool found, GameState *gameState) {
-    if (found) {
-        printf("Congratulations! %s guessed the number %d in %d attempts.\n",
-               gameState->playerType == HUMAN ? "You" : "The computer", randomNumber, 
-               gameState->attempts);
-    } else {
-        printf("Game Over! The number was %d.\n", randomNumber);
-    }
-    printf("%s guesses were: ", gameState->playerType == HUMAN ? "Your" : "The computer");
-    for (int i = 0; i < gameState->attempts; i++) {
-        printf("%d ", gameState->guesses[i]);
-    }
-    printf("\n");
+  free(gameState->guesses);
+  free(gameState);
 }
 
 int getInput() {
-    char buffer[20];
-    int input = 0;
-    scanf("%s", buffer);
-    input = atoi(buffer);
-    return input;
+  char buffer[20];
+  int input = 0;
+  scanf("%s", buffer);
+  input = atoi(buffer);
+  return input;
+}
+
+int _requestInput(int min, int max) {
+  printf("Enter your guess (%d-%d): ", min, max);
+  return getInput();
+}
+
+int _getNextNumber(int result, GameState *gameState) {
+  if (gameState->attempts < 1) {
+    return (gameState->min + gameState->max) / 2;
+  }
+  int lastGuess = gameState->guesses[gameState->attempts - 1];
+  if (result < 0) {
+    gameState->min = lastGuess + 1;
+    return (gameState->min + gameState->max) / 2;
+  } else {
+    gameState->max = lastGuess - 1;
+    return (gameState->min + lastGuess) / 2;
+  }
+}
+// a game that only gives X number of choices
+// track the number of attempts
+// i want to track the guesses
+bool runGame(int randomNumber, GameState *gameState) {
+  int guess = 0;
+  int result = 0;
+  while (gameState->attempts < gameState->maxAttempts) {
+    if (gameState->playerType == COMPUTER) {
+      guess = _getNextNumber(result, gameState);
+    } else {
+      guess = _requestInput(gameState->min, gameState->max);
+    }
+    gameState->guesses[gameState->attempts++] = guess;
+
+    result = guess - randomNumber;
+    if (result == 0) {
+      return true;
+    } else if (gameState->playerType == HUMAN) {
+      feedback(result);
+    }
+  }
+  return false;
 }
 
 void feedback(int state) {
-    if (state < 0) {
-        printf("Too low! Try again.\n");
-    } else if (state > 0) {
-        printf("Too high! Try again.\n");
-    } 
+  if (state < 0) {
+    printf("Too low! Try again.\n");
+  } else if (state > 0) {
+    printf("Too high! Try again.\n");
+  }
 }
 
-int requestInput(int min, int max) {
-    printf("Enter your guess (%d-%d): ", min, max);
-    return getInput();
+void giveFeedback(int randomNumber, bool found, GameState *gameState) {
+  if (found) {
+    printf("Congratulations! %s guessed the number %d in %d attempts.\n",
+           gameState->playerType == HUMAN ? "You" : "The Computer",
+           randomNumber, gameState->attempts);
+  } else {
+    printf("Game Over! The number was %d.\n", randomNumber);
+  }
+  printf("%s guesses were: ",
+         gameState->playerType == HUMAN ? "Your" : "The computer's");
+  for (int i = 0; i < gameState->attempts; i++) {
+    printf("%d ", gameState->guesses[i]);
+  }
+  printf("\n\n");
 }
 
-// uses a binary search
-int _getNextNumber(int result, GameState *gameState) {
-
-    int lastGuess = gameState->guesses[gameState->attempts - 1];
-    if (result < 0) {
-        
-        gameState->min = lastGuess + 1; // Update min to last guess + 1
-        return (gameState->min + gameState->max) / 2; // Return the new guess
-        
-    } else {
-        gameState->max = lastGuess - 1;
-        return (gameState->min + lastGuess) / 2;
-    }
-}
-
-
-bool runGame(int randomNumber, GameState *gameState) {
-    int guess = 0;
-    int result = 0;
-    while(gameState->attempts < gameState->maxAttempts) {
-
-        if (gameState->playerType == COMPUTER) {
-            if (gameState->attempts > 0) {
-                guess = _getNextNumber(result, gameState);
-            } else {
-                guess = (gameState->min + gameState->max) / 2;
-            }
-        } else {
-            guess = requestInput(DEFAULT_MIN, DEFAULT_MAX);
-        }
-        gameState->guesses[gameState->attempts++] = guess;
-
-        result = guess - randomNumber;
-        if (result == 0) {
-            return true; // found the number
-        } else if (gameState->playerType == HUMAN) {
-            feedback(result);
-        }
-    }
-    return false; // failed the game
-}
-
-void printMenu() {
-    printf("\n");
-    printf("Play Game\n");
-    printf("Exit\n");
-    printf("Enter your choice (play | exit): ");
+void _printMenu() {
+  printf("Play Game\n"); // play or exit
+  printf("Exit\n");
+  printf("Enter your choice (play | exit): ");
 }
 
 void start() {
-    printf("Welcome to the Guessing Game!\n");
-    char choice[20];
+  printf("Welcome to the Guessing Game!\n");
+  char choice[20];
 
-    while (strcasecmp(choice, "exit") != 0) {
-        printMenu();
-        scanf("%s", choice);
+  while (strcasecmp(choice, "exit") != 0) {
+    _printMenu();
+    scanf("%s", choice);
 
-        if (strcasecmp(choice, "play") == 0) {
-            int randomNumber = getRandomNumber(DEFAULT_MIN, DEFAULT_MAX);
-            printf("Starting the game...\n");
+    if (strcasecmp(choice, "play") == 0) {
+      printf("Starting the game...\n");
+      int randomNumber = getRandomNumber(DEFAULT_MIN, DEFAULT_MAX);
+      GameState *human = initializeGameState(DEFAULT_MAX_ATTEMPTS, HUMAN,
+                                             DEFAULT_MIN, DEFAULT_MAX);
+      bool found = runGame(randomNumber, human);
+      giveFeedback(randomNumber, found, human);
 
-            GameState *gameState = initializeGameState(DEFAULT_MAX_ATTEMPTS, HUMAN, DEFAULT_MIN, DEFAULT_MAX);
-            bool found = runGame(randomNumber, gameState);
-            giveFeedback(randomNumber, found, gameState);
+      GameState *computer = initializeGameState(DEFAULT_MAX_ATTEMPTS, COMPUTER,
+                                                DEFAULT_MIN, DEFAULT_MAX);
+      found = runGame(randomNumber, computer);
+      giveFeedback(randomNumber, found, computer);
 
-            
-            GameState *computerGameState = initializeGameState(DEFAULT_MAX_ATTEMPTS, COMPUTER, DEFAULT_MIN, DEFAULT_MAX);
-            found = runGame(randomNumber, computerGameState);
-            giveFeedback(randomNumber, found, computerGameState);
-            
-            if(computerGameState->attempts < gameState->attempts) {
-                printf("The computer won!\n");
-            } else if (computerGameState->attempts > gameState->attempts) {
-                printf("You won!\n");
-            } else {
-                printf("It's a tie!\n");
-            }
-            
-            freeGameState(computerGameState); // Free the allocated memory for computer game state
-            freeGameState(gameState); // Free the allocated memory for game state
+      if (computer->attempts < human->attempts) {
+        printf("The computer won!\n");
+      } else if (computer->attempts > human->attempts) {
+        printf("You won!\n");
+      } else {
+        printf("It's a tie!\n");
+      }
 
-        } else if (strcasecmp(choice, "exit") == 0) {
-            printf("Exiting the game...\n");
-        } else {
-            printf("Invalid choice. Please try again.\n");
-        }
+      freeGameState(human);
+      freeGameState(computer);
+
+    } else if (strcasecmp(choice, "exit") == 0) {
+      printf("Exiting the game...\n");
+    } else {
+      printf("Invalid choice. Please try again.\n");
     }
+  }
 }
--- CandtheHeap/solutionCode/GuessingGame/TestDriver.c
+++ CandtheHeap/solutionCode/GuessingGame/TestDriver.c
@@ -12,10 +12,53 @@
 #include <stdlib.h>
 #include <time.h>
 
+void testMallocAndFree() {
+  GameState *one = initializeGameState(10, 0, 0, 100);
+  GameState *two = initializeGameState(10, 1, 0, 100);
+
+  freeGameState(one);
+  freeGameState(two);
+}
+
+void testGetInput() {
+  printf("Enter a value: ");
+  int myValue = getInput();
+  printf("Testing my value by entering 10, the results is %d\n\n", myValue);
+}
+
+void testFeedback() {
+  int state = 50 - 48;
+  feedback(state);
+  state = 23 - 48;
+  feedback(state);
+}
+
+void testGiveFeedback() {
+  GameState *human = initializeGameState(10, HUMAN, 0, 100);
+  human->attempts = 1;
+  human->guesses[0] = 30;
+
+  GameState *computer = initializeGameState(10, COMPUTER, 0, 100);
+  computer->attempts = 2;
+  computer->guesses[0] = 15;
+  computer->guesses[1] = 3;
+
+  giveFeedback(30, true, human);
+  giveFeedback(99, false, computer);
+
+  freeGameState(human);
+  freeGameState(computer);
+}
+
 int main() {
   srand(0); // Seed the random number generator with a fixed value for
-             // reproducibility
+            // reproducibility
   int randomNumber = getRandomNumber(DEFAULT_MIN, DEFAULT_MAX);
   printf("Random number generated: %d\n", randomNumber);
+
+  // testGetInput();
+  // testMallocAndFree();
+  // testFeedback();
+  testGiveFeedback();
   return 0;
 }
