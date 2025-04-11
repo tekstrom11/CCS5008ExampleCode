8b586edc72fa0b10484c7191d2539b76f928fbb2
Albert Lionelle <albert.lionelle@colostate.edu>
2025-04-10 19:46:26 -0600

    updated from code along

--- CandtheHeap/solutionCode/ReadingList.c
+++ CandtheHeap/solutionCode/ReadingList.c
@@ -5,11 +5,7 @@
  * @date: 2025.04.10
  */
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-typedef struct {
+ typedef struct {
   char author[100];
   char title[256];
   int year;
@@ -29,21 +25,11 @@ ReadingList *createReadingList(int size) {
   return readingList;
 }
 
-void copyString(char *dest, const char *src, int size) {
-  strncpy(dest, src, size - 1);
-  dest[size - 1] = '\0'; // Ensure null-termination
+void freeReadingList(ReadingList *readingList) {
+  free(readingList->books);
+  free(readingList);
 }
 
-void addBook(ReadingList *readingList, const char *author, const char *title,
-             int year) {
-  int index = readingList->current;
-  copyString(readingList->books[index].author, author,
-             sizeof(readingList->books[index].author));
-  copyString(readingList->books[index].title, title,
-             sizeof(readingList->books[index].title));
-  readingList->books[index].year = year;
-  readingList->current++;
-}
 void printReadingList(ReadingList *readingList) {
   for (int i = 0; i < readingList->current; i++) {
     printf("Book %d: %s by %s (%d)\n", i + 1, readingList->books[i].title,
@@ -51,35 +37,44 @@ void printReadingList(ReadingList *readingList) {
   }
 }
 
-void removeBook(ReadingList *readingList, int index) {
-  if (index < 0 || index >= readingList->current) {
-    printf("Invalid index\n");
-    return;
-  }
-  for (int i = index; i < readingList->current - 1; i++) {
-    readingList->books[i] = readingList->books[i + 1];
-  }
-  readingList->current--;
+void copyString(char *dest, const char *src, int size) {
+    strncpy(dest, src, size - 1);
+    dest[size - 1] = '\0';
 }
 
-void freeLibrary(ReadingList *library) {
-  free(library->books);
-  free(library);
+void addBook(ReadingList *readingList, const char *author, const char *title, int year) {
+    int index = readingList->current;
+    Book *singleBook = &readingList->books[index];
+    copyString(singleBook->author, author, sizeof(singleBook->author));
+    copyString(singleBook->title, title, sizeof(singleBook->title));
+    singleBook->year = year;
+    readingList->current++;
+}
+
+void removeBook(ReadingList *readingList, int index) {
+    if (index < 0 || index >= readingList->current) {
+        printf("Invalid index\n");
+        return;
+    }
+    for(int i = index; i < readingList->current - 1; i++) {
+        readingList->books[i] = readingList->books[i+1];
+    }
+    readingList->current--;
 }
 
 int main() {
-  ReadingList *library = createReadingList(5);
+  ReadingList *myList = createReadingList(10);
 
-  addBook(library, "Brandon Sanderson", "Mistborn: The Final Empire", 2006);
-  addBook(library, "Frank Herbert", "Dune", 1965);
-  addBook(library, "J.R.R. Tolkien", "The Fellowship of the Ring", 1954);
+  addBook(myList, "Brandon Sanderson", "Mistborn: The Final Empire", 2006);
+  addBook(myList, "Frank Herbert", "Dune", 1965);
+  addBook(myList, "J.R.R. Tolkien", "The Fellowship of the Ring", 1954);
 
-  printReadingList(library);
+  printReadingList(myList);
   printf("\n");
-  removeBook(library, 1); // Remove the book at index 1 (Dune)
-  printReadingList(library);
+  removeBook(myList, 1);
+  printReadingList(myList);
 
-  freeLibrary(library);
 
+  freeReadingList(myList);
   return 0;
 }
