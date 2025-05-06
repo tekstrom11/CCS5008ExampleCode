07a67956a07080cdf18c68f066a7fcdfa5c9b1be
Albert Lionelle <albert.lionelle@colostate.edu>
2025-05-05 22:24:01 -0600

    fixed headers

--- SequentialDataStructures/solutionCode/NeuQueue.c
+++ SequentialDataStructures/solutionCode/NeuQueue.c
@@ -9,14 +9,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#include "NeuQueue.h"
-
 #include <errno.h>
-#include <stdbool.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
 #include "NeuQueue.h"
 
