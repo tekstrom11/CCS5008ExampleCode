842026bf29855903e8db0200a7c35231bc292173
Albert Lionelle <lionelle@gmail.com>
2025-05-07 20:34:09 -0600

    initial solution

--- SortedStructuresTrees/solutionCode/NeuTree.c
+++ SortedStructuresTrees/solutionCode/NeuTree.c
@@ -15,8 +15,23 @@
  * @return A pointer to the newly created tree.
  */
 NeuTree* create_tree() {
-    // TODO: Implement
-    return NULL;
+    NeuTree* tree = (NeuTree*)malloc(sizeof(NeuTree));
+    if (tree == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    tree->root = NULL;
+    tree->size = 0;
+    return tree;
+}
+
+void __free_tree_nodes(NeuNode* node) {
+    if (node == NULL) {
+        return;
+    }
+    __free_tree_nodes(node->left);
+    __free_tree_nodes(node->right);
+    free(node);
 }
 
 /**
@@ -24,7 +39,11 @@ NeuTree* create_tree() {
  * @param tree A pointer to the tree to free.
  */
 void free_tree(NeuTree* tree) {
-    // TODO: Implement
+    if (tree == NULL) {
+        return;
+    }
+    __free_tree_nodes(tree->root);
+    free(tree);
 }
 
 /**
@@ -33,7 +52,35 @@ void free_tree(NeuTree* tree) {
  * @param data The data to add to the tree.
  */
 void add(NeuTree* tree, char data) {
-    // TODO: Implement
+    NeuNode* new_node = (NeuNode*)malloc(sizeof(NeuNode));
+    if (new_node == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        exit(EXIT_FAILURE);
+    }
+    new_node->data = data;
+    new_node->left = NULL;
+    new_node->right = NULL;
+
+    if (tree->root == NULL) {
+        tree->root = new_node;
+    } else {
+        NeuNode* current = tree->root;
+        NeuNode* parent = NULL;
+        while (current != NULL) {
+            parent = current;
+            if (data < current->data) {
+                current = current->left;
+            } else {
+                current = current->right;
+            }
+        }
+        if (data < parent->data) {
+            parent->left = new_node;
+        } else {
+            parent->right = new_node;
+        }
+    }
+    tree->size++;
 }
 
 /**
@@ -42,7 +89,54 @@ void add(NeuTree* tree, char data) {
  * @param visit A function pointer to apply to each node's data.
  */
 void breadth_first_traversal(NeuTree* tree, void (*visit)(char)) {
-    // TODO: Implement
+    if (tree == NULL || tree->root == NULL) {
+        return;
+    }
+
+    NeuNode* queue[tree->size]; // = (NeuNode**)malloc(tree->size * sizeof(NeuNode*));
+    
+    int front = 0;
+    int rear = 0;
+
+    queue[rear++] = tree->root;
+
+    while (front < rear) {
+        NeuNode* current = queue[front++];
+        visit(current->data);
+        if (current->left != NULL) {
+            queue[rear++] = current->left;
+        }
+        if (current->right != NULL) {
+            queue[rear++] = current->right;
+        }
+    }
+ }
+
+void __preorder(NeuNode *node, void (*visit)(char)) {
+    if (node == NULL) {
+        return;
+    }
+    visit(node->data);
+    __preorder(node->left, visit);
+    __preorder(node->right, visit);
+}
+
+void __inorder(NeuNode *node, void (*visit)(char)) {
+    if (node == NULL) {
+        return;
+    }
+    __inorder(node->left, visit);
+    visit(node->data);
+    __inorder(node->right, visit);
+}
+
+void __postorder(NeuNode *node, void (*visit)(char)) {
+    if (node == NULL) {
+        return;
+    }
+    __postorder(node->left, visit);
+    __postorder(node->right, visit);
+    visit(node->data);
 }
 
 /**
@@ -52,5 +146,22 @@ void breadth_first_traversal(NeuTree* tree, void (*visit)(char)) {
  * @param visit A function pointer to apply to each node's data.
  */
 void depth_first_traversal(NeuTree* tree, enum TraversalType type, void (*visit)(char)) {
-    // TODO: Implement
+    if (tree == NULL || tree->root == NULL) {
+        return;
+    }
+
+    switch (type) {
+        case PRE_ORDER:
+            __preorder(tree->root, visit);
+            break;
+        case IN_ORDER:
+            __inorder(tree->root, visit);
+            break;
+        case POST_ORDER:
+            __postorder(tree->root, visit);
+            break;
+        default:
+            fprintf(stderr, "Invalid traversal type\n");
+            exit(EXIT_FAILURE);
+    }
 }
