5006f90b8cb053856d35d34e721445e909ec82c5
Albert Lionelle <lionelle@gmail.com>
2025-05-07 22:37:48 -0600

    initial starter code

---
+++ SortedStructuresTrees/starterCode/NeuHeap.c
@@ -0,0 +1,51 @@
+/**
+* Starter code for Code Along Heap
+*
+**/
+
+#include "NeuHeap.h"
+
+/**
+ * Creates a new heap with the given capacity.
+ * @param capacity The maximum number of elements the heap can hold.
+ * @return A pointer to the newly created heap.
+ */
+NeuHeap* create_heap(int capacity) {
+    // TODO: Implement
+    return NULL;
+}
+
+/**
+ * Frees the memory allocated for the heap.
+ * @param heap A pointer to the heap to free.
+ */
+void free_heap(NeuHeap* heap) {
+    // TODO: Implement
+}
+
+/**
+ * Adds a new element to the heap.
+ * @param heap A pointer to the heap.
+ * @param value The value to add to the heap.
+ */
+void enqueue(NeuHeap* heap, int value) {
+    // TODO: Implement
+}
+
+/**
+ * Removes and returns the highest-priority element from the heap.
+ * @param heap A pointer to the heap.
+ * @return The value of the highest-priority element.
+ */
+int dequeue(NeuHeap* heap) {
+    // TODO: Implement
+    return 0;
+}
+
+/**
+ * Prints the elements of the heap.
+ * @param heap A pointer to the heap.
+ */
+void print_heap(NeuHeap* heap) {
+    // TODO: Implement
+}
