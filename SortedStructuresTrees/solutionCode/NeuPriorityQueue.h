cd351a21e2a034c3cc8ceebaddf6b314d5d29b99
Albert Lionelle <lionelle@gmail.com>
2025-05-07 18:45:03 -0600

    initial header

---
+++ SortedStructuresTrees/solutionCode/NeuPriorityQueue.h
@@ -0,0 +1,26 @@
+#ifndef NEU_PRIORITY_QUEUE_H
+#define NEU_PRIORITY_QUEUE_H
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct NeuPriorityQueue {
+    int *data;
+    int capacity;
+    int size;
+} NeuPriorityQueue;
+
+NeuPriorityQueue* create_priority_queue(int capacity);
+void free_priority_queue(NeuPriorityQueue* queue);
+void enqueue(NeuPriorityQueue* queue, int value);
+int dequeue(NeuPriorityQueue* queue);
+int peek(NeuPriorityQueue* queue);
+bool has_items(NeuPriorityQueue* queue);
+void clear_priority_queue(NeuPriorityQueue* queue);
+void print_priority_queue(NeuPriorityQueue* queue);
+
+
+
+
+
+#endif // NEU_PRIORITY_QUEUE_H
